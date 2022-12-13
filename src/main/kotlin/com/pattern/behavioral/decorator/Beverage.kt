ec2d754ca92401e590202c56f42f6183d9b04bc9
Sony Jacob Thomas <sonyjtp@gmail.com>
2022-12-12 22:15:38 -0600

    Updated decorator

--- src/main/kotlin/com/pattern/behavioral/decorator/Beverage.kt
+++ src/main/kotlin/com/pattern/behavioral/decorator/Beverage.kt
@@ -1,44 +1,41 @@
 package com.pattern.behavioral.decorator
 
 interface Beverage{
-    fun getDescription(): String  = "Basic"
+    fun getDescription(): String
     fun cost(): Double
 }
 
 object Espresso: Beverage {
     private const val description: String = "Espresso"
-    override fun getDescription(): String  = description
+    override fun getDescription(): String  = this.description
 
     override fun cost()  = 1.99
 }
 
 object HouseBlend: Beverage {
     private const val description: String = "HouseBlend"
-    override fun getDescription(): String  = description
+    override fun getDescription(): String  = this.description
     override fun cost()  = .89
 }
 
-sealed class CondimentDecorator : Beverage {
-    abstract override fun getDescription(): String
+sealed interface CondimentDecorator : Beverage
 
-}
-
-class Milk(private val beverage: Beverage): CondimentDecorator() {
+class Milk(private val beverage: Beverage): CondimentDecorator {
     override fun getDescription()  = beverage.getDescription() + ", Milk"
     override fun cost() = beverage.cost() + .25
 }
 
-class Mocha(private val beverage: Beverage): CondimentDecorator() {
+class Mocha(private val beverage: Beverage): CondimentDecorator {
     override fun getDescription()  = beverage.getDescription() + ", Mocha"
     override fun cost() = beverage.cost() + .2
 }
 
-class Soy(private val beverage: Beverage): CondimentDecorator() {
+class Soy(private val beverage: Beverage): CondimentDecorator {
     override fun getDescription()  = beverage.getDescription() + ", Soy"
     override fun cost() = beverage.cost() + .15
 }
 
-class Whip(private val beverage: Beverage): CondimentDecorator() {
+class Whip(private val beverage: Beverage): CondimentDecorator {
     override fun getDescription()  = beverage.getDescription() + ", Whip"
     override fun cost() = beverage.cost() + .18
 }
